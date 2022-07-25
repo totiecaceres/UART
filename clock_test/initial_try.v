e2bbfd467a16576abaa16c59c7e512eb0fb34b56
Mark Unity Toledo Caceres <104969784+totiecaceres@users.noreply.github.com>
2022-07-25 15:58:20 +0800

    Add files via upload

--- clock_test/initial_try.v
+++ clock_test/initial_try.v
@@ -1,21 +1,23 @@
-module initial_try(clk, nrst, tx, count, bit_count, clk_pulse, state);
+module initial_try(clk, nrst, data_in, tx);
 	input clk;
 	input nrst;
+	input [31:0] data_in;
 	output tx;
-	output [10:0] count;
-	output [3:0] bit_count;
-	output clk_pulse;
-	output [1:0] state;
-	parameter [7:0] data = 8'b01010100;
-	parameter baud = 9600; //
-	parameter freq = 12000000; //clock speed f=1/s
+	//output [10:0] count;s
+	//output [3:0] bit_count;
+	//output clk_pulse;
+	//output [1:0] state;
+	//reg [31:0] data = {8'h53,8'h6E,8'h61,8'h70};
+	//assign data = data_in;
+	parameter baud = 9600; //1/9600 = 104.1667 ms
+	parameter freq = 12000000; //clock speed f=1/s, 83.333ns
 	parameter lim = freq/baud;
 	//12M/9.6k = 1250 bits ; log2(1250) = 10.2877 or 11 bits [000 0000 0000] counter
 	//determine the number of count that will generate in 30Mhz freq at 9.6k bit per sec
 	//log2(1250) = 10.2877 or 11 bits so,
 	reg [10:0] count = 11'd0; //clk_div
 	reg [3:0] bit_count = 4'b0000; //req. 10 bit per frame so, it has 4 bit
-	reg tx=1;
+	reg tx;
 	reg clk_pulse=0;
 	reg [1:0] state = 0;
 	
@@ -23,7 +25,8 @@ module initial_try(clk, nrst, tx, count, bit_count, clk_pulse, state);
 		begin
 			if(!nrst)begin
 				bit_count <= 0;
-				count <= 11'b0; end
+				count <= 11'b0;
+			end
 			else begin
 				//different condition: if idle/transition state, do not pulse the next start bit.
 				if(count!=11'd1249)begin
@@ -61,16 +64,48 @@ module initial_try(clk, nrst, tx, count, bit_count, clk_pulse, state);
 		end
 	end
 	
-	always @(state or bit_count)
+	reg [31:0] data;
+	initial begin
+		data <= data_in[31:0];
+	end
+	
+	reg start_flag;
+	reg stop_flag;
+	always @(state or bit_count or nrst)
 	begin
-		if(state==2'b11 && bit_count!=0)begin
-			tx <= data[bit_count-1];
-		end
-		else if(state==2'b01)begin
-			//tx <= 0;
-		end
-		else begin
+		if(!nrst) begin
 			tx <= 1;
+		end	
+		else begin
+			if(state==2'b01)begin // start bit
+				tx <= 0;
+				start_flag <= 1;
+			end
+			else if(state==2'b11)begin // sending..
+				if(start_flag==1)begin
+					tx <= data[0];
+					data <= data >> 1 | {tx,32'b0};
+					if(bit_count!=8)begin
+						stop_flag <= 0;
+					end
+					else begin
+						stop_flag <= 1;
+					end
+				end
+				else begin
+					//
+				end
+			end
+			else begin					// stop bit
+				if(stop_flag==1)begin
+					tx <= 0;
+					start_flag <= 1;
+				end
+				else begin
+					tx <= 1;
+					start_flag <= 0;
+				end
+			end
 		end
 	end
 		  
--- clock_test/tb_initial_try.v
+++ clock_test/tb_initial_try.v
@@ -1,18 +1,22 @@
 module tb_initial_try;
 	reg clk;
 	reg nrst;
-	wire tx=1;
-	wire [10:0] count;
-	wire [3:0] bit_count;
-	wire clk_pulse;
+	reg [31:0] data_in = {8'h53,8'h6E,8'h61,8'h70};
+	wire tx;
+	//wire [10:0] count;
+	//wire [3:0] bit_count;
+	//wire clk_pulse;
+	//wire [1:0] state;
 
 	initial_try uut(
 		.clk(clk),
 		.nrst(nrst),
-		.tx(tx),
-		.count(count),
-		.bit_count(bit_count),
-		.clk_pulse(clk_pulse)
+		.data_in(data_in),
+		.tx(tx)
+		//.count(count),
+		//.bit_count(bit_count),
+		//.clk_pulse(clk_pulse),
+		//.state(state)
 	);
 	always
 		#4200 assign clk = ~clk; // 12Mhz --> 83.33ns per full cyc --> 41.666ns per half period
@@ -21,8 +25,12 @@ module tb_initial_try;
 		nrst = 0;
 		#21000000;
 		nrst =1;
-		#420000000;
-		nrst =0;
+		#210000000;
+		nrst =1;
+		#210000000;
+		nrst =1;
+		#210000000;
+		nrst = 0;
 		#20;
 		
 	end
