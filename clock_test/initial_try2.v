92f23ad750a433b6eaf16243ab787b84ec6848d4
Mark Unity Toledo Caceres <104969784+totiecaceres@users.noreply.github.com>
2022-07-26 17:16:25 +0800

    Add files via upload

--- clock_test/initial_try2.v
+++ clock_test/initial_try2.v
@@ -1,51 +1,122 @@
-module initial_try2(clk, nrst, tx, count, bit_count);
+module initial_try2(clk, nrst, data_in, tx, count, bit_count, clk_pulse, state);
 	input clk;
 	input nrst;
+	input [7:0] data_in;
 	output tx;
 	output [10:0] count;
 	output [3:0] bit_count;
-	parameter [7:0] data = 8'b01010100;
-	parameter baud = 9600; //
-	parameter freq = 12000000; //clock speed f=1/s
-	parameter lim = freq/baud;
+	output clk_pulse;
+	output [1:0] state;
+	//reg [31:0] data = {8'h53,8'h6E,8'h61,8'h70};
+	//assign data = data_in;
+	parameter baud = 9600; //1/9600 = 104.1667 ms
+	parameter freq = 12000000; //clock speed f=1/s, 83.333ns
+	parameter lim = (freq/baud);
 	//12M/9.6k = 1250 bits ; log2(1250) = 10.2877 or 11 bits [000 0000 0000] counter
 	//determine the number of count that will generate in 30Mhz freq at 9.6k bit per sec
 	//log2(1250) = 10.2877 or 11 bits so,
-	
 	reg [10:0] count = 11'd0; //clk_div
-	wire clk_pulse;
 	reg [3:0] bit_count = 4'b0000; //req. 10 bit per frame so, it has 4 bit
 	reg tx;
-	always @(posedge clk)
+	reg clk_pulse;
+	reg [1:0] state = 0;
+	reg [7:0] data;
+	always @(posedge clk or nrst)
 		begin
 			if(!nrst)begin
-				tx <= 1;
 				bit_count <= 0;
-				count <= 11'b0; end
+				count <= 11'b0;
+				clk_pulse <= 1;
+			end
 			else begin
 				//different condition: if idle/transition state, do not pulse the next start bit.
-				count <= count +11'b1;
-				if(count==11'd1250)begin
-					//new bit
-					//tx <= 0; // start bit
-						if(bit_count==4'd9)begin
-							tx <= 1; //end bit
-							bit_count <= 0;
-						end
-						else if(bit_count>0 & bit_count<4'd9)begin
-							integer i = bit_count;
-							tx <= data[i-1]; 	
-						end
-						else begin
-							tx <= 0;
-						end
-					count <= 11'b0;
-					bit_count <= bit_count + 4'b1;
+				if(count!=lim-1)begin
+					count <= count +11'b1;
+					if(count==lim/2)begin
+						clk_pulse = ~clk_pulse;
+					end
 				end
 				else begin
-					tx <= 1; //active high
+					if(bit_count!=4'd9)begin
+						bit_count <= bit_count + 4'b1;
+					end
+					else begin
+						bit_count <= 0;
+					end
+					clk_pulse = ~clk_pulse;
+					count <= 11'b0;
 				end
 			end
 		end
-					  
+	
+	always @(bit_count or nrst)
+	begin
+		if(!nrst)begin
+			state <= 2'b00;
+		end
+		else begin
+			if(bit_count==4'd0)begin // start bit
+			state <= 2'b01;
+			end
+			else if(bit_count==4'd9)begin
+				state <= 2'b10;
+			end
+			else begin
+				state <= 2'b11;
+			end
+		end
+	end
+	//reg [7:0] data;
+	reg start_flag;
+	reg stop_flag;
+	initial begin
+		start_flag <= 1;
+		stop_flag <= 1;
+	end
+	
+	always @(state or bit_count or nrst or data_in)
+	begin
+		if(!nrst) begin
+			tx <= 1;
+		end	
+		else begin
+			if(state==2'b01)begin // start bit
+				tx <= 0;
+				start_flag <= 1;
+				data <= data_in; // niddle
+			end
+			else if(state==2'b11)begin // sending..
+				if(start_flag==1)begin
+					tx <= data[0];
+					if(bit_count!=8)begin
+						stop_flag <= 0;
+						data <= data >> 1;
+					end
+					else begin
+						stop_flag <= 1;
+					end
+				end
+				//else begin
+					//data <= 8'h59;
+				//end
+			end
+			else if(state==2'b10) begin					// stop bit
+				if(stop_flag==1)begin
+					//tx <= 0;
+					start_flag <= 1;
+				end
+				else begin
+					tx <= 1;
+					start_flag <= 0;
+					data <= data_in;
+				end
+			end
+			else begin
+				tx <= 1;
+				start_flag <= 1;
+				stop_flag <= 1;
+			end
+		end
+	end
+		  
 endmodule
---
+++ clock_test/tb_initial_try2.v
@@ -0,0 +1,73 @@
+module tb_initial_try2;
+	reg clk;
+	reg nrst;
+	reg [7:0] data_in;
+	wire tx;
+	wire [10:0] count;
+	wire [3:0] bit_count;
+	wire clk_pulse;
+	wire [1:0] state;
+	integer i;
+	initial_try2 uut(
+		.clk(clk),
+		.nrst(nrst),
+		.data_in(data_in),
+		.tx(tx),
+		.count(count),
+		.bit_count(bit_count),
+		.clk_pulse(clk_pulse),
+		.state(state)
+	);
+	always
+		#4167 assign clk = ~clk; // 12Mhz --> 83.33ns per full cyc --> 41.666ns per half period
+	
+	initial begin
+		clk = 0;
+		nrst = 0;
+		data_in = 8'b0;
+		#2150100;
+		nrst =1;
+		for(i=0; i<100; i=i+1)begin
+			assign data_in = $urandom%256;
+			#104175000;
+		end
+		//assign data_in = 8'h53;
+		//#104164179;
+		//assign data_in = 8'h6E;
+		//#104175000;
+		//assign data_in = 8'h61;
+		//#104175000;
+		//assign data_in = 8'h70;
+		//#104175000;
+		//assign data_in = 8'h53;
+		//#104164179;
+		//assign data_in = 8'h6E;
+		//#104175000;
+		//assign data_in = 8'h61;
+		//#104175000;
+		//assign data_in = 8'h70;
+		//#104175000;
+		//nrst = 0;
+		//#2150100;
+		//nrst =1;
+		//assign data_in = 8'h53;
+		//#104164179;
+		//assign data_in = 8'h6E;
+		//#104175000;
+		//assign data_in = 8'h61;
+		//#104175000;
+		//assign data_in = 8'h70;
+		//#104175000;
+		//assign data_in = 8'h53;
+		//#104164179;
+		//assign data_in = 8'h6E;
+		//#104175000;
+		//assign data_in = 8'h61;
+		//#104175000;
+		//assign data_in = 8'h70;
+		//#104175000;
+		//nrst = 0;
+		//#20;
+		
+	end
+endmodule
