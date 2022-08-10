40a6864768bbc33e5470f21841a34d63ddc1d926
Mark Unity Toledo Caceres <104969784+totiecaceres@users.noreply.github.com>
2022-08-10 11:15:56 +0800

    Add files via upload

--- UART_tx_rx.v
+++ UART_tx_rx.v
@@ -1,21 +1,25 @@
-module UART_tx_rx(clk, nrst, data, ready, tx, data_store, bit_count, bit_count2, byte_count, busy, busy2, idle, done, signal, bit_count3, data_store2, busy1);
+module UART_tx_rx(clk, nrst, rx, tx, rx2, tx2);
+//module UART_tx_rx(clk, nrst, rx, ready, tx, data_store, bit_count, bit_count2, byte_count, busy, busy2, idle, done, bit_count3, data_store2, busy1);
 	input clk;
 	input nrst;
-	input data;
-	output ready;
+	input rx;
+	output rx2;
+	//output ready;
 	output tx;
-	output [9:0] data_store;
-	output [7:0] bit_count;
-	output [3:0] bit_count2;
-	output [13:0] byte_count;
-	output busy;
-	output busy2;
-	output idle;
-	output done;
-	output signal;
-	output [4:0] bit_count3;
-	output [31:0] data_store2 = 32'b0;
-	output busy1;
+	output tx2;
+	//output [9:0] data_store;
+	//output [3:0] bit_count;
+	//output [3:0] bit_count2;
+	//output [13:0] byte_count;
+	//output busy;
+	//output busy2;
+	//output idle;
+	//output done;
+	//output [4:0] bit_count3;
+	//output [31:0] data_store2 = 32'b0;
+	//output busy1;
+	assign rx2 = rx;
+	assign tx2 = tx;
 	
 	reg [9:0] data_store = 10'b0;
 	reg [31:0] data_store2 = 32'b0;
@@ -30,26 +34,26 @@ module UART_tx_rx(clk, nrst, data, ready, tx, data_store, bit_count, bit_count2,
 	//determine the number of count that will generate in 30Mhz freq at 9.6k bit per sec
 	//log2(1250) = 10.2877 or 11 bits so,
 	reg [10:0] count = 11'd0; //clk_div
-	reg [7:0] bit_count = 8'b00000000; //req. 10 bit per frame so, it has 4 bit
+	reg [3:0] bit_count = 4'b0000; //req. 10 bit per frame so, it has 4 bit
 	reg [13:0] byte_count = 0;
 	reg [3:0] byte_count2 = 0;
-	reg busy = 0;
+	//reg busy = 0;
 	reg busy2 = 0;
 	reg idle = 0;
 	reg tx; 
 	reg busy1 = 0;
 	reg [1:0] state = 0;
 	reg done = 0;
-	reg signal = 0;
 	reg tx_read=1;
 	reg ready=0;
 	reg [10:0] count2 = 11'd0;
 	reg [3:0] bit_count2 = 4'b0000;
 	reg [10:0] count3 = 11'd0;
 	reg [4:0] bit_count3 = 5'b00000;
+	assign busy = (busy1 ^ busy2);
 	
 /////////////////////////////////////////////////////////////////////////////////////////////////	
-	always@(negedge signal or negedge nrst)begin
+	always@(negedge idle or negedge nrst)begin
 		if(!nrst || busy)begin
 			byte_count <= 0;
 		end
@@ -63,25 +67,10 @@ module UART_tx_rx(clk, nrst, data, ready, tx, data_store, bit_count, bit_count2,
 			end
 		end
 	end
-/////////////////////////////////////////////////////////////////////////////////////////////////	
-	always @(posedge clk or negedge nrst)begin
-		if(!nrst)begin
-			tx_read <= 1;
-		end
-		else begin
-			if(data_store!=10'b1111111111)begin
-				tx_read <= 0;
-			end
-			else begin
-				tx_read <= 1;
-			end
-			ready <= tx_read && !data;
-		end
-	end
 /////////////////////////////////////////////////////////////////////////////////////////////////
+	//busy <= busy1 ^ busy2;
 	always @(posedge clk or negedge nrst)
 		begin
-			busy <= busy1 ^ busy2;
 			/////////////////////////////////////////////////////////////////////////////////////////////////
 			if(!nrst || busy)begin
 				count <= 11'b0;
@@ -91,7 +80,7 @@ module UART_tx_rx(clk, nrst, data, ready, tx, data_store, bit_count, bit_count2,
 			end
 			else begin
 				/////////////////////////////////////////////////////////////////////////////////////////////////
-				if(signal==1)begin
+				if(idle==1)begin
 					if(count2!=lim-1)begin
 						count2 <= count2 + 11'b1;
 					end
@@ -99,7 +88,7 @@ module UART_tx_rx(clk, nrst, data, ready, tx, data_store, bit_count, bit_count2,
 						count2 <= 11'b0;
 						if(bit_count2==4'd8)begin
 							bit_count2 <= 0;
-							signal <= ~signal;
+							idle <= ~idle;
 						end
 						else begin
 							bit_count2 <= bit_count2 +1;
@@ -111,21 +100,39 @@ module UART_tx_rx(clk, nrst, data, ready, tx, data_store, bit_count, bit_count2,
 					bit_count2 <= 0;
 				end
 				/////////////////////////////////////////////////////////////////////////////////////////////////
+				if(!nrst)begin
+					tx_read <= 1;
+				end
+				else begin
+					if(data_store!=10'b1111111111)begin
+						tx_read <= 0;
+					end
+					else begin
+						tx_read <= 1;
+					end
+					ready <= tx_read && !rx;
+				end
+				/////////////////////////////////////////////////////////////////////////////////////////////////
 				if(count!=lim-1)begin
 					count <= count +11'b1;
 				end
 				else begin
 					count <= 11'b0;
-					data_store <= data_store << 1 | {10'b0000000000, data};
+					data_store <= data_store << 1 | {10'b0000000000, rx};
 					if(bit_count==8)begin
 						data_store2 <= data_store2 << 8 | {24'd0,data_store[7:0]}; 
 					end
 					if(ready)begin
 						bit_count <= 0;
-						signal <= ~signal;
+						idle <= ~idle;
 					end
 					else begin
-						bit_count <= bit_count + 1;
+						if(bit_count!=15)begin
+							bit_count <= bit_count + 1;
+						end
+						else begin
+							bit_count <= bit_count;
+						end
 					end
 				end
 			end
